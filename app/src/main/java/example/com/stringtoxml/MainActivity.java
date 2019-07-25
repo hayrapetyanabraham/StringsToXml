f6b42b4c612180a3743ea6c7abac31509100e2cd
Abraham <hayrapetyanabraham@gmail.com>
2019-07-25 17:37:42 +0400

    Added new languages

--- app/src/main/java/example/com/stringtoxml/MainActivity.java
+++ app/src/main/java/example/com/stringtoxml/MainActivity.java
@@ -1,10 +1,11 @@
 package example.com.stringtoxml;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.Log;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,39 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "TEXT_KEY_LOG";
-    String textAll = "";
+    private EditText editTextLikeXml;
+    private String textAll = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> keys = stringsTexts(R.raw.keys);
-        List<String> russian = stringsTexts(R.raw.russian);
-        List<String> rusianKeyValue = new ArrayList<>();
-
-
-        Spanned sp = Html.fromHtml( "&#34;");
-
-        for (int i = 0; i < keys.size(); i++) {
-           // String value = "<string name=" + "\\u0022" + keys.get(i) + "\\u0022" + ">" + russian.get(i) + "</string>";
-            String value = "<string name=" + sp + keys.get(i) + sp + ">" + russian.get(i) + "</string>";
-            Log.d("KEY_STRING", value);
-            rusianKeyValue.add(value);
-        }
-
-
-        for (int i = 0; i < rusianKeyValue.size(); i++) {
-            textAll = textAll + rusianKeyValue.get(i);
-
-        }
-        Log.d("TEXT_XML", textAll);
-
-        Log.d(TAG, " SIZE " + rusianKeyValue.size());
-        Log.d(TAG, " Values " + rusianKeyValue.toString());
+        editTextLikeXml = findViewById(R.id.edit_text_like_xml);
+        logGreek();
     }
 
+    private void logGreek() {
+        List<String> greekKeys = stringsTexts(R.raw.greek_keys);
+        List<String> greek = stringsTexts(R.raw.greek);
+        List<String> stringArrayList = new ArrayList<>();
+        Spanned spanned = Html.fromHtml("&#34;");
+        for (int i = 0; i < greekKeys.size(); i++) {
+            String value = "<string name=" + spanned + greekKeys.get(i) + spanned + ">" + greek.get(i) + "</string>" + "\n";
+            stringArrayList.add(value);
+        }
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            textAll = textAll + stringArrayList.get(i);
+        }
+        editTextLikeXml.setText(textAll);
+        Log.d("TEXT_XML", textAll);
+    }
 
     private List<String> stringsTexts(int resourceId) {
         List<String> keys = new ArrayList<>();
@@ -56,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
             reader = new BufferedReader(new InputStreamReader(file));
             String line = reader.readLine();
             while (line != null) {
-                Log.d("StackOverflow", line);
                 keys.add(line);
                 line = reader.readLine();
             }
@@ -65,35 +58,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return keys;
     }
-
-
-    public static String UnicodeToString(String Hex) {
-        String enUnicode = null;
-        String deUnicode = null;
-
-        for (int i = 0; i < Hex.length(); i++) {
-            if (enUnicode == null)
-                enUnicode = String.valueOf(Hex.charAt(i));
-            else
-                enUnicode = enUnicode + Hex.charAt(i);
-
-            if (i % 4 == 3) {
-                if (enUnicode != null) {
-                    if (deUnicode == null)
-                        deUnicode = String.valueOf((char) Integer.valueOf(
-                                enUnicode, 16).intValue());
-                    else
-                        deUnicode = deUnicode
-                                + String.valueOf((char) Integer.valueOf(
-                                enUnicode, 16).intValue());
-                }
-
-                enUnicode = null;
-            }
-        }
-
-        return deUnicode;
-    }
-
 
 }
